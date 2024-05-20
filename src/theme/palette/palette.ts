b2ecb16fd850967b9f9a6769cfe48a50b19db966
Panteleimonow <nicolas.panteleimonow@gmail.com>
2024-05-20 16:26:06 +0200

    feat: added theme

---
+++ src/theme/palette/palette.ts
@@ -0,0 +1,11 @@
+export const palette = {
+    primary: {
+        main: '#25994b',
+        dark: '#005722',
+        light: '#c2e4ca',
+        contrastText: '#fff',
+    },
+    background: {
+        default: '#fafafa'
+    }
+}

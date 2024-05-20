af65ef7f585e27821df46216eecafcc7f43e7f60
Panteleimonow <nicolas.panteleimonow@gmail.com>
2024-05-20 17:05:06 +0200

    fix: added eslint prettier config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-      <Box>
-        <Typography variant={"h1"}>
-          Hello
-        </Typography>
-      </Box>
+    <Box>
+      <Typography variant={"h1"}>Hello</Typography>
+    </Box>
   );
 }

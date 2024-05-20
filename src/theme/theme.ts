07d64984f1a2a47e3bdbedacb645d1698c2b10a2
Panteleimonow <nicolas.panteleimonow@gmail.com>
2024-05-20 16:26:06 +0200

    feat: added theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+import { palette } from "./palette/palette";
+import { typography } from "./typography/typography";
+
+export const theme = createTheme({
+    palette,
+    typography,
+    components: {
+        MuiButton: {
+            defaultProps: {
+                variant: 'contained',
+                size: 'large'
+            },
+        }
+    }
+})
---
+++ src/theme/typography/typography.ts
@@ -0,0 +1,55 @@
+export const typography = {
+    fontFamily: 'Poppins, sans-serif',
+    h1: {
+        fontSize: '3.0rem',
+        fontWeight: 600
+    },
+    h2: {
+        fontSize: '2.5rem',
+        fontWeight: 600
+    },
+    h3: {
+        fontSize: '2rem',
+        fontWeight: 600
+    },
+    h4: {
+        fontSize: '1.5rem',
+        fontWeight: 600
+    },
+    h5: {
+        fontSize: '1.25rem',
+        fontWeight: 600
+    },
+    h6: {
+        fontSize: '1rem',
+        fontWeight: 600
+    },
+    subtitle1: {
+        fontSize: '1rem',
+        fontWeight: 600
+    },
+    subtitle2: {
+        fontSize: '0.875rem',
+        fontWeight: 600
+    },
+    body1: {
+        fontSize: '1rem',
+        fontWeight: 400
+    },
+    body2: {
+        fontSize: '0.875rem',
+        fontWeight: 400
+    },
+    button: {
+        fontSize: '1rem',
+        fontWeight: 600
+    },
+    caption: {
+        fontSize: '0.75rem',
+        fontWeight: 400
+    },
+    overline: {
+        fontSize: '0.75rem',
+        fontWeight: 600
+    }
+}

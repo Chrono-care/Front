6dc3fc1f7b94491381499d2252adfc8fddd46f4b
emmerandesassis <emmeran.desassis@epitech.eu>
2024-05-28 16:27:18 +0200

    feat theme and layout

--- src/app/components/modules/Header/Header.tsx
+++ src/app/components/modules/Header/Header.tsx
@@ -1,79 +1,37 @@
 'use client';
 
-import {
-    alpha,
-    AppBar,
-    Box,
-    Container,
-    IconButton,
-    TextField,
-    Toolbar,
-    Typography,
-    useTheme,
-} from '@mui/material';
-import { Login, Menu, Search } from '@mui/icons-material';
+import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import { Login, Menu } from '@mui/icons-material';
 
 export const Header = () => {
-    const theme = useTheme();
     return (
-        <Container component={'header'} maxWidth={'lg'}>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar variant={'elevation'}>
-                    <Toolbar>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <Menu />
+        <Box component={'header'}>
+            <Box>
+                <AppBar
+                    position="sticky"
+                    sx={{
+                        background: '#FFFFFF',
+                        boxShadow: 0,
+                    }}
+                >
+                    <Toolbar
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        <IconButton>
+                            <Menu color={'inherit'}>Menu</Menu>
                         </IconButton>
-                        <Box
-                            display={'flex'}
-                            alignItems={'center'}
-                            justifyContent={'space-evenly'}
-                            flexGrow={1}
-                        >
-                            <Typography variant="h6" component="div">
-                                Chrono-care
-                            </Typography>
-                            <TextField
-                                placeholder={'Recherche'}
-                                InputProps={{
-                                    endAdornment: (
-                                        <IconButton size={'small'}>
-                                            <Search />
-                                        </IconButton>
-                                    ),
-                                }}
-                                size={'small'}
-                                sx={{
-                                    '&:hover': {
-                                        backgroundColor: alpha(
-                                            theme.palette.primary.light,
-                                            0.75,
-                                        ),
-                                    },
-                                    borderColor: theme.palette.primary.dark,
-                                    background: theme.palette.primary.light,
-                                    borderRadius: 2,
-                                }}
-                            />
-                        </Box>
-                        <Box
-                            flexGrow={4}
-                            marginLeft={2}
-                            display={'flex'}
-                            justifyContent={'flex-end'}
-                        >
-                            <IconButton size={'small'}>
-                                <Login />
-                            </IconButton>
-                        </Box>
+                        <Typography variant="h6" component="div">
+                            Chrono-care
+                        </Typography>
+                        <IconButton size={'small'}>
+                            <Login />
+                        </IconButton>
                     </Toolbar>
                 </AppBar>
             </Box>
-        </Container>
+        </Box>
     );
 };

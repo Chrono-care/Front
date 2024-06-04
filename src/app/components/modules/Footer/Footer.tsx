c4abfd6d60f079483fdb6dc980fb341da890c3aa
Panteleimonow <nicolas.panteleimonow@gmail.com>
2024-06-04 15:03:43 +0200

    feat: changed the header icon for the research section and added subtexts to specify what the icon does. The footer is now a stack

--- src/app/components/modules/Footer/Footer.tsx
+++ src/app/components/modules/Footer/Footer.tsx
@@ -1,22 +1,30 @@
 'use client';
 
-import {
-    BottomNavigation,
-    BottomNavigationAction,
-    Box,
-    Divider,
-} from '@mui/material';
+import { Box, Divider, Link, Stack, useTheme } from '@mui/material';
 
 export const Footer = () => {
+    const theme = useTheme();
     return (
-        <Box component={'footer'}>
-            <BottomNavigation showLabels>
-                <BottomNavigationAction label="Recents" />
-                <Divider orientation="vertical" variant="middle" flexItem />
-                <BottomNavigationAction label="Favorites" />
-                <Divider orientation="vertical" variant="middle" flexItem />
-                <BottomNavigationAction label="Nearby" />
-            </BottomNavigation>
+        <Box
+            sx={{ backgroundColor: theme.palette.background.paper }}
+            padding={2}
+            display={'flex'}
+            justifyContent={'center'}
+            component={'footer'}
+        >
+            <Stack direction="row" spacing={4}>
+                <Link href={'/'} color="inherit" underline={'none'}>
+                    FAQ
+                </Link>
+                <Divider orientation={'vertical'} />
+                <Link href={'/'} color="inherit" underline={'none'}>
+                    CGU
+                </Link>
+                <Divider orientation={'vertical'} />
+                <Link href={'/'} color="inherit" underline={'none'}>
+                    HELP
+                </Link>
+            </Stack>
         </Box>
     );
 };
--- src/app/components/modules/Header/Header.tsx
+++ src/app/components/modules/Header/Header.tsx
@@ -1,16 +1,25 @@
 'use client';
 
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
-import { Login, Menu } from '@mui/icons-material';
+import {
+    AppBar,
+    Box,
+    IconButton,
+    Link,
+    Toolbar,
+    Typography,
+    useTheme,
+} from '@mui/material';
+import { Menu, Search } from '@mui/icons-material';
 
 export const Header = () => {
+    const theme = useTheme();
     return (
         <Box component={'header'}>
             <Box>
                 <AppBar
                     position="sticky"
                     sx={{
-                        background: '#FFFFFF',
+                        background: theme.palette.background.paper,
                         boxShadow: 0,
                     }}
                 >
@@ -20,15 +29,49 @@ export const Header = () => {
                             justifyContent: 'space-between',
                         }}
                     >
-                        <IconButton>
-                            <Menu color={'inherit'}>Menu</Menu>
-                        </IconButton>
-                        <Typography variant="h6" component="div">
+                        <Box display="flex">
+                            <IconButton size={'small'}>
+                                <Menu color={'inherit'} />
+                            </IconButton>
+                            <Typography
+                                display={'flex'}
+                                alignItems={'center'}
+                                variant={'body2'}
+                                marginLeft={1}
+                            >
+                                Menu
+                            </Typography>
+                        </Box>
+                        <Link
+                            color={theme.palette.primary.dark}
+                            href={'/'}
+                            underline={'none'}
+                            variant="h6"
+                        >
                             Chrono-care
-                        </Typography>
-                        <IconButton size={'small'}>
-                            <Login />
-                        </IconButton>
+                        </Link>
+                        <Box display="flex">
+                            <IconButton
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    ':hover': {
+                                        background: 'none',
+                                    },
+                                }}
+                                size={'small'}
+                            >
+                                <Search />
+                            </IconButton>
+                            <Typography
+                                variant={'body2'}
+                                display={'flex'}
+                                alignItems={'center'}
+                                marginLeft={1}
+                            >
+                                Rechercher
+                            </Typography>
+                        </Box>
                     </Toolbar>
                 </AppBar>
             </Box>

88f189a3575cd85b3f06c07ef4df8e0167cd59e5
emmerandesassis <emmeran.desassis@epitech.eu>
2024-06-17 16:26:13 +0200

    refacto: Modification des noms

--- src/app/components/modules/Login/loginFormValidationSchema.ts
+++ src/app/components/modules/Login/loginFormValidationSchema.ts
@@ -1,10 +1,8 @@
 import * as Yup from 'yup';
 
-const validationSchema = Yup.object({
+export const loginFormValidationSchema = Yup.object({
     email: Yup.string()
         .email('Adresse email invalide')
         .required("L'email est requis"),
     password: Yup.string().required('Mot de passe est requis'),
 });
-
-export default validationSchema;
